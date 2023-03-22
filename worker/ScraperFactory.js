db490fe5f5d891f8376da7a1c01ae881cd077cef
broch016 <broch016@umn.edu>
2023-03-21 22:52:35 -0500

    Created ScraperFactory object, converted to an API vs monolith

---
+++ worker/ScraperFactory.js
@@ -0,0 +1,14 @@
+import AllRecipesScraper from './AllRecipes/AllRecipesScraper.js'
+
+class ScraperFactory {
+    create(scraperType, doc, url) {
+        switch(scraperType) {
+            case 'AllRecipes':
+                return new AllRecipesScraper(doc, url);
+            default:
+                return new Error('Scraper type not supported');
+        }
+    }
+}
+
+export default ScraperFactory
